9dd9a32d8997829c202df5383ec37874958bf567
shankarmb <shankar575.b@gmail.com>
2018-05-03 22:32:12 +0530

    asdasd

--- model.py
+++ model.py
@@ -480,8 +480,9 @@ class DCGAN(object):
                             # with open(log_file_name, 'a') as log_file:
                             #     log_file.write("[Sample] d_loss: %.8f, g_loss: %.8f" % (d_loss, g_loss) + '\n')
                             #     print('Saved Sample loss details to the log file..')
-                        except:
+                        except Exception as ex:
                             print("one pic error!...")
+                            print(ex)
 
                 if np.mod(counter, 500) == 2:
                     self.save(config.checkpoint_dir, counter)
@@ -491,7 +492,7 @@ class DCGAN(object):
             # make a list and insert and pop or leave it as it is..
 
             os.makedirs('cropped_sample_images/' + self.dataset_name, exist_ok=True)
-            if epoch > 100 and epoch % 50 == 0:
+            if epoch >= 100 and epoch % 50 == 0:
                 try:
                     samples, _, _ = self.sess.run(
                         [self.sampler, self.d_loss, self.g_loss],
